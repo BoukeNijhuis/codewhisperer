cb3917616c46707cc2aac8981641878c89fbd599
Bouke Nijhuis <bouke.nijhuis@gmail.com>
2023-04-16 15:14:52 +0200

    Initial commit

---
+++ src/main/java/PrimeNumberGenerator.java
@@ -0,0 +1,10 @@
+public class PrimeNumberGenerator {
+
+    /**
+     * Generate prime numbers up to a given number.
+     * @param n The upper bound of the prime numbers to generate.
+     * @return An array of prime numbers.
+     */
+
+}
+
