755ccff446d862465a121ebbf968b1c594a2a68a
Bouke Nijhuis <bouke.nijhuis@gmail.com>
2023-04-22 14:35:00 +0200

    updated array to a list

--- src/main/java/PrimeNumberGenerator.java
+++ src/main/java/PrimeNumberGenerator.java
@@ -3,7 +3,7 @@ public class PrimeNumberGenerator {
     /**
      * Generate prime numbers up to a given number.
      * @param n The upper bound of the prime numbers to generate.
-     * @return An array of prime numbers.
+     * @return A list of prime numbers.
      */
 
 }
